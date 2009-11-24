b285be4875cbaefc08d91bd63dd9565c3bbbfa3a
Dmitri Goutnik <dg@syrec.org>
2009-11-24 23:59:33 +0800

    fixed error formatting

--- Support/RubyMate/run_script.rb
+++ Support/RubyMate/run_script.rb
@@ -45,10 +45,12 @@ TextMate::Executor.run(cmd, :version_args => ["--version"]) do |str, type|
           "</a> in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}<br/>"
         elsif line =~ /(\[[^\]]+\]\([^)]+\))\s+\[([\w\_\/\.]+)\:(\d+)\]/
           spec, file, line = $1, $2, $3, $4
-          "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{spec}</span>:#{line}<br/>"
+          file = File.join(ENV['TM_PROJECT_DIRECTORY'], file) unless file =~ /^\//
+          "<a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{spec}:#{line}</a><br/>"
         elsif line =~ /([\w\_]+).*\[([\w\_\/\.]+)\:(\d+)\]/
           method, file, line = $1, $2, $3
-          "<span><a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{method}</span>:#{line}<br/>"
+          file = File.join(ENV['TM_PROJECT_DIRECTORY'], file) unless file =~ /^\//
+          "<a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(file)}&amp;line=#{line}\">#{File.basename(file)}:#{line}</a><br/>"
         elsif line =~ /^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors\b.*/
           "<span style=\"color: #{$1 + $2 == "00" ? "green" : "red"}\">#{$&}</span><br/>"
         else
